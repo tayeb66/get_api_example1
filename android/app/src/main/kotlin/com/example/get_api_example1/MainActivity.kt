b620eded8628ed5573579d777c971ef54b37aca2
Tayeb Ahmed <tayebahmed97@gmail.com>
2023-11-04 17:20:03 +0600

    initial

---
+++ android/app/src/main/kotlin/com/example/get_api_example1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.get_api_example1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Fetching data "),
+        centerTitle: true,
+      ),
+    );
+  }
+}
+
